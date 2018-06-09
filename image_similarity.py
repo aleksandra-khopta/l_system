1916fe4cab72f087bbc8f33f4b67b155db8db12f
sasha <z@z.com>
2018-06-09 19:43:17 +0300

    L-systems. The great beginning.

---
+++ image_similarity.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+# For Sasha from Dima with love
+
+
+_DEBUG = False
+
+
+def _phase_correlation(a, b):
+    G_a = np.fft.fft2(a)
+    G_b = np.fft.fft2(b)
+    conj_b = np.ma.conjugate(G_b)
+    R = G_a*conj_b
+    R /= np.absolute(R)
+    r = np.fft.ifft2(R).real
+    return r
+
+
+def _analyze_correlation(correlogram, range=5):
+    rolled_correlogram = np.roll(np.roll(correlogram, range, axis=0), range, axis=1)
+    if _DEBUG:
+        cv2.imshow("Rolled", 5 *rolled_correlogram)
+    return np.max(rolled_correlogram[:2*range, :2*range])
+
+
+def _scale_image(image, w, h):
+    return cv2.resize(image, (w, h), interpolation=cv2.INTER_NEAREST)
+
+
+def _to_float(image):
+    res = np.float32(image)
+    res = 1.0 - res / 255.0
+    return res
+
+
+def is_sample_in_image(image, sample, kernel_size=3):
+    h, w = sample.shape[:2]
+    scaled_image = _scale_image(image, w, h)
+
+    if _DEBUG:
+        cv2.imshow("Scaled", scaled_image)
+
+    kernel = np.ones((kernel_size, kernel_size), np.float32)
+
+    pattern = _to_float(scaled_image)
+    # dilated_pattern = cv2.dilate(pattern, kernel)
+
+    candidate = _to_float(sample)
+    dilated_candidate = cv2.dilate(candidate, kernel)
+
+    overlap = np.multiply(pattern, dilated_candidate)
+
+    if _DEBUG:
+        cv2.imshow("Pattern", pattern)
+        cv2.imshow("Candidate", dilated_candidate)
+        cv2.imshow("Overlap", overlap)
+
+        print("overlap sum = {:.2f}".format(np.sum(overlap)))
+        print("pattern sum = {:.2f}".format(np.sum(pattern)))
+
+    corr = _phase_correlation(pattern, candidate)
+
+    if _DEBUG:
+        cv2.imshow("Corr", 5 * corr)
+        print("Max correleation = {}".format(np.max(corr)))
+
+    # max_correlation_around = _analyze_correlation(corr, range=3)
+
+    # return np.sum(overlap) / np.sum(pattern), max_correlation_around
+    return np.sum(overlap) / np.sum(pattern), 0
+
+#
+# origin_file = "fractal.jpg"
+# sample_file = "subimage4.png"
+#
+# origin = cv2.imread(origin_file)
+# sample = cv2.imread(sample_file)
+#
+# similarity, corr = is_sample_in_image(origin, sample, kernel_size=5)
+# print("Similarity = {:.2f}".format(similarity))
+# print("Correlation = {:.2f}".format(corr))
+#
+# cv2.waitKey()
--- l_systems.py
+++ l_systems.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import math
 import image_similarity
+import sys
+
+sys.setrecursionlimit(50000)
 
 
 def load_image(path):
@@ -25,9 +28,31 @@ def load_image(path):
     return image[top:bottom, left:right]
 
 
+_counter = 0
+
+
+def _show_debug_image(window_name, input_image, points_mask, point, wait_time=-1):
+    global _counter
+    _counter += 1
+    if _counter % 20 == 0:
+    # if True:
+        res_image = np.stack([input_image, input_image, input_image], axis=-1)
+        res_image[points_mask == 1] = (0, 0, 200)
+        cv2.circle(res_image, point, 3, (200, 0, 0), thickness=2)
+        cv2.imwrite("debug.png", res_image)
+        cv2.imshow(window_name, res_image)
+
+        # cv2.imshow("Matches", points_mask)
+
+        cv2.waitKey(wait_time)
+
+
 # image_src = cv2.imread("fractal.jpg")
 image_src = load_image("fractal.png")
+# visited = np.zeros_like(image_src)
+# matches = np.zeros_like(image_src)
 visited = np.zeros(image_src.shape[:2])
+matches = np.zeros(image_src.shape[:2])
 h, w = image_src.shape[:2]
 
 
@@ -59,86 +84,13 @@ def subimage_best(image, bottom_center, theta, scale):
     center_y = bottom_y - (height // 2) * math.cos(theta)
     center = (int(center_x), int(center_y))
 
-
-    dbg_image = np.copy(image)
-    cv2.circle(dbg_image, center, 3, (0, 0, 255), thickness=3)
-    cv2.circle(dbg_image, bottom_center, 3, (0, 200, 200), thickness=3)
-#    cv2.imshow("Debug", dbg_image)
-#    cv2.waitKey()
-
-
     v_x = (math.cos(theta), math.sin(theta))
     v_y = (-math.sin(theta), math.cos(theta))
     s_x = center[0] - v_x[0] * (width / 2) - v_y[0] * (height / 2)
     s_y = center[1] - v_x[1] * (width / 2) - v_y[1] * (height / 2)
     mapping = np.array([[v_x[0],v_y[0], s_x], [v_x[1],v_y[1], s_y]])
-    # if image is None:
-    #     print("Error")
-    # else:
-    #     print("shape for warp = {}".format(str(image.shape)))
-
     return cv2.warpAffine(image, mapping, (width, height), flags=cv2.WARP_INVERSE_MAP, borderMode=cv2.BORDER_REPLICATE)
 
-subimage1 = subimage(image_src, (310, 270), 25, w // 2, h // 2)
-subimage2 = subimage(image_src, (200, 270), -25, w // 2, h // 2)
-subimage3 = subimage(image_src, (255, 130), 0, w // 2, h // 2)
-origin45 = subimage(image_src, (w // 2, h // 2), 45, w, h)
-subimage4 = subimage_best(image_src, (255, 385), 25, 0.5)
-cv2.imwrite("subimage1.jpg", subimage1)
-cv2.imwrite("subimage2.jpg", subimage2)
-cv2.imwrite("subimage3.jpg", subimage3)
-cv2.imwrite("subimage4.jpg", subimage4)
-cv2.imwrite("origin45.jpg", origin45)
-
-subimages = [[310, 270, 25, w // 2, h //2], [200, 270, -25, w // 2, h //2], [255, 130, 0, w // 2, h //2]]
-
-#Clock-wise from the bottom center
-
-subimages_num = 3
-subimages_bb = {}
-#get bounding boxes
-#get start bb index
-# lowest_bb_index = 0;
-# min_bb_y = 100000;
-# for i in range(1, subimages_num)  :
-#     width = subimages[i][3]
-#     height = subimages[i][4]
-#     bb_left = subimages[i][1] - width // 2
-#     bb_right = subimages[i][1] + width // 2
-#     bb_bottom = subimages[i][1] + height // 2
-#     bb_top = subimages[i][1] - height // 2
-#     if (bb_bottom < min_bb_y):
-#         min_bb_y = bb_bottom
-#         lowest_bb_index = i
-#     subimages_bb.add({bb_left, bb_top, bb_right, bb_bottom})
-
-#if not symmetric - start is always from bottom
-
-# l_system1 = {""}
-# l_system2 = {""}
-# for i in range(1, subimages_num)  :
-#     center = (subimages[i][0],subimages[i][1])
-#     theta = subimages[i][2]
-#     width = subimages[i][3]
-#     height = subimages[i][4]
-#     if (theta > 0):
-#         l_system1.add("+")
-#     elif (theta < 0):
-#         l_system1.add("-")
-#     l_system1.add("X")
-#     n = w // width
-#     m = h // height
-#     if (m == n):
-#         for j in range(1, n):
-#             l_system2.add("F")
-
-#####################################################
-# image = Image.open("image.jpg") #Открываем изображение.
-# draw = ImageDraw.Draw(image) #Создаем инструмент для рисования.
-# width = image.size[0] #Определяем ширину.
-# height = image.size[1] #Определяем высоту.
-# pix = image.load() #Выгружаем значения пикселей.
-# image.save("res.jpg", "JPEG")
 
 class color(object):
     def __init__(self, r, g, b):
@@ -152,10 +104,10 @@ def equal(image, subimage):
 
 def find_elem(x_start, y_start):
     bottom_center = (x_start, y_start)
-    # for theta in range(-90, 90):
-    for theta in [-25.0, 0.0, 25.0]:
-        # for scale in np.arange(0.1, 0.9, 0.1):
-        for scale in [0.5]:
+    theta_range = [-25, 0, 25]
+    scale_range = [0.5]
+    for theta in theta_range:
+        for scale in scale_range:
             sub_img = subimage_best(image_src, bottom_center, theta, scale)
             if equal(image_src, sub_img):
                 return True, theta, scale
@@ -163,7 +115,7 @@ def find_elem(x_start, y_start):
 
 
 l_system1 = []
-
+_POSSIBLE_MOVES = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 def bypass(back_color, x, y):
     if x < 0 or y < 0 or x >= w or y >= h:
@@ -172,18 +124,18 @@ def bypass(back_color, x, y):
         return
     if image_src[y, x] != back_color:
         visited[y, x] = 1
+        _show_debug_image("Visited", visited, matches, (x, y), wait_time=1)
         is_sub_img, theta, scale = find_elem(x, y)
         if is_sub_img:
+            matches[y, x] = 1
             if theta > 0:
                 l_system1.append("+")
             elif theta < 0:
                 l_system1.append("-")
             l_system1.append("X")
             ######## TO DO : search in the frame ##########
-            bypass(back_color, x + 1, y)
-            bypass(back_color, x - 1, y)
-            bypass(back_color, x, y + 1)
-            bypass(back_color, x, y - 1)
+        for (dy, dx) in _POSSIBLE_MOVES:
+            bypass(back_color, x + dx, y + dy)
 
 print(image_src.shape)
 print(visited.shape)
@@ -192,7 +144,7 @@ last_row = image_src[-1, :]
 x = np.argmin(last_row)
 
 bypass(255, x, h - 1)
-# print(l_system1)
+print(l_system1)
 
 cv2.imshow("Input", image_src)
 cv2.imshow("Visited", visited)
