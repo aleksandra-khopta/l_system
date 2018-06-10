4b31473c5e0901a066226a0024065a3a00507332
dima <d.yu.vasiliev@gmail.com>
2018-06-10 15:27:56 +0300

    Add 4 images for testing.

--- test.py
+++ test.py
@@ -4,11 +4,14 @@ import find_matches
 import l_systems
 
 
-_TEST_IMAGES = ["images/fractal-1.png"]
-_TEST_LSYSTEMS = ["gt/l-system-1.txt"]
+_TEST_CASES = range(5)
+_TEST_IMAGES = ["images/fractal{}.png".format(i) for i in _TEST_CASES]
+_TEST_LSYSTEMS = ["gt/l-system-{}.txt".format(i) for i in _TEST_CASES]
 
 
 def main():
+    success_cases = 0
+    total_cases = 0
     for image_file, gt_file in zip(_TEST_IMAGES, _TEST_LSYSTEMS):
         print(">>>>> Evaluate {} <<<<<".format(image_file))
         match_structures = find_matches.process_image(image_file)
@@ -21,8 +24,16 @@ def main():
         print("Calculated L-system:")
         print(l_system_str)
         print("-------------------------")
-        print("Success = {}".format(gt_str == l_system_str))
+        if gt_str == l_system_str:
+            success_cases += 1
+            print("*** Success ***")
+        else:
+            print("!!! Failure !!!")
+        total_cases += 1
         print("-------------------------")
+
+    print("============ Results =============")
+    print("{} of {} test cases pass.".format(success_cases, total_cases))
 
 
 if __name__ == "__main__":
