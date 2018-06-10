953f791443f8b6c507e8a3e797dff3dca6c7a831
dima <d.yu.vasiliev@gmail.com>
2018-06-10 22:43:17 +0300

    Add major angle pre-calculation.

--- find_matches.py
+++ find_matches.py
@@ -8,42 +8,32 @@ from match_structures import *
 from image_similarity import is_sample_in_image
 
 _POSSIBLE_MOVES = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-_RANGE_THETA = [-25, 0, 25]
-_RANGE_SCALE = [0.5]
+_RANGE_SCALE = [0.33, 0.5]
 
 
-def find_matches_at_point(image_src, x_start, y_start, threshold):
+_FOUND_SCALE = []
+_FOUND_D_THETA = []
+
+
+def find_matches_at_point(image_src, x_start, y_start, threshold, angle_range):
+    global _FOUND_SCALE
+    global _FOUND_D_THETA
     matches = []
     bottom_center = x_start, y_start
-    for theta in _RANGE_THETA:
-        for scale in _RANGE_SCALE:
-            sub_img = subimage_best(image_src, bottom_center, theta, scale)
-            score = is_sample_in_image(image_src, sub_img)
-            if score > threshold:
-                matches.append(Match(theta, scale, score))
+    for scale in _FOUND_SCALE or _RANGE_SCALE:
+        for theta in angle_range:
+            for d_theta in _FOUND_D_THETA or np.arange(-2.0, 2.0, 1.0):
+                sub_img = subimage_best(image_src, bottom_center, theta + d_theta, scale)
+                score = is_sample_in_image(image_src, sub_img)
+                if score > threshold:
+                    matches.append(Match(theta + d_theta, scale, score))
+                    if not _FOUND_SCALE:
+                        _FOUND_SCALE = [scale]
+                        _FOUND_D_THETA = [d_theta]
+                        print("Scale = {}".format(scale))
+                        print("Theta = {}".format(theta))
+                        break
     return matches
-
-
-# def bypass(image_src, visited, matches, back_color, x, y):
-#     h, w = image_src.shape[:2]
-#     if x < 0 or y < 0 or x >= w or y >= h:
-#         return
-#     if visited[y, x] == 1:
-#         return
-#     if image_src[y, x] != back_color:
-#         visited[y, x] = 1
-#         show_debug_image("Visited", visited, matches, (x, y), wait_time=1)
-#         is_sub_img, theta, scale = find_elem(image_src, x, y)
-#         if is_sub_img:
-#             matches[y, x] = 1
-#             if theta > 0:
-#                 l_system1.append("+")
-#             elif theta < 0:
-#                 l_system1.append("-")
-#             l_system1.append("X")
-#             ######## TO DO : search in the frame ##########
-#         for (dy, dx) in _POSSIBLE_MOVES:
-#             bypass(image_src, visited, matches, back_color, x + dx, y + dy)
 
 
 def valid_position(p, image, background_color, visited):
@@ -52,7 +42,7 @@ def valid_position(p, image, background_color, visited):
     return 0 <= y < h and 0 <= x < w and visited[p] == 0 and image[p] != background_color
 
 
-def search_matches(image_src, start, back_color, similarity_threshold, visual=False):
+def search_matches(image_src, start, back_color, similarity_threshold, angle_range, visual=False):
     visited = np.zeros(image_src.shape[:2])
     match_mask = np.zeros(image_src.shape[:2])
     distances = -np.ones(image_src.shape[:2])
@@ -70,7 +60,7 @@ def search_matches(image_src, start, back_color, similarity_threshold, visual=Fa
         y, x = q.popleft()
         if visual:
             show_debug_image("Visited", visited, match_mask, (x, y), wait_time=1)
-        point_matches = find_matches_at_point(image_src, x, y, similarity_threshold)
+        point_matches = find_matches_at_point(image_src, x, y, similarity_threshold, angle_range)
         if point_matches:
             match_mask[y, x] = 1
             matches[(y,x)] = point_matches
@@ -93,10 +83,23 @@ def find_start(image_src):
     return h-1, x
 
 
-def process_image(input_image, background_color=255, similarity_threshold=0.8, dump_folder=None, visual=False):
+def generate_angle_range(angle):
+    angle_range = [0]
+    current_angle = angle
+    while current_angle < 90:
+        angle_range.insert(0, -current_angle)
+        angle_range.append(current_angle)
+        current_angle += angle
+    return angle_range
+
+
+def process_image(input_image, background_color=255, similarity_threshold=0.7, dump_folder=None, visual=False):
     image_src = load_image(input_image)
     start = find_start(image_src)
-    match_structures, dbg_image = search_matches(image_src, start, background_color, similarity_threshold)
+    major_theta = calculate_major_angle(image_src)
+    angle_range = generate_angle_range(major_theta)
+    print(angle_range)
+    match_structures, dbg_image = search_matches(image_src, start, background_color, similarity_threshold, angle_range, visual=visual)
 
     if dump_folder:
         dump_structures(dump_folder, match_structures)
@@ -120,7 +123,8 @@ def main():
     parser.add_argument('--visual', type=bool, default=False, help='Enable visualization')
 
     args = parser.parse_args()
-    process_image(args.input_image, args.background_color, args.similarity_threshold, args.dump_folder)
+    print(args)
+    process_image(args.input_image, args.background_color, args.similarity_threshold, args.dump_folder, visual=args.visual)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -4,31 +4,56 @@ import find_matches
 import l_systems
 
 
-_TEST_CASES = range(5)
+# _TEST_CASES = range(5)
+_TEST_CASES = [0, 3]
 _TEST_IMAGES = ["images/fractal{}.png".format(i) for i in _TEST_CASES]
 _TEST_LSYSTEMS = ["gt/l-system-{}.txt".format(i) for i in _TEST_CASES]
+
+
+def compare_systems(lsys1, lsys2):
+    print(lsys1)
+    print(lsys2)
+
+    angle1 = float(lsys1[0])
+    angle2 = float(lsys2[0])
+    if abs(angle1 - angle2) > 2.0:
+        return False
+    return lsys1[1:] == lsys2[1:]
+
+
+def load_lsystem(file):
+    with open(file) as f:
+        angle = f.readline()
+        axiom = f.readline()
+        rule1 = f.readline()
+        rule2 = f.readline()
+        return [angle, axiom, rule1, rule2]
 
 
 def main():
     success_cases = 0
     total_cases = 0
-    for image_file, gt_file in zip(_TEST_IMAGES, _TEST_LSYSTEMS):
+    for tc, image_file, gt_file in zip(_TEST_CASES, _TEST_IMAGES, _TEST_LSYSTEMS):
         print(">>>>> Evaluate {} <<<<<".format(image_file))
-        match_structures = find_matches.process_image(image_file)
-        l_system_str = l_systems.process_structures(match_structures)
-        gt_str = open(gt_file).read()
+
+        match_structures = find_matches.process_image(image_file, dump_folder="debug/t{}".format(tc))
+        l_system = l_systems.process_structures(match_structures)
+        gt_lsystem = load_lsystem(gt_file)
+
         print("-------------------------")
         print("Ground truth L-system:")
-        print(gt_str)
+        print(l_systems.lsystem_to_str(gt_lsystem))
         print("-------------------------")
         print("Calculated L-system:")
-        print(l_system_str)
+        print(l_systems.lsystem_to_str(l_system))
         print("-------------------------")
-        if gt_str == l_system_str:
+
+        if compare_systems(gt_lsystem, l_system):
             success_cases += 1
             print("*** Success ***")
         else:
             print("!!! Failure !!!")
+
         total_cases += 1
         print("-------------------------")
 
