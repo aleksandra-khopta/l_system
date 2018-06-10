00a8b61bbbda3b9fcc3695b2637edcb727af4e42
dima <d.yu.vasiliev@gmail.com>
2018-06-10 11:41:01 +0300

    Add filtering of matches.

--- l_systems.py
+++ l_systems.py
@@ -19,11 +19,21 @@ def main():
     labaled_array, features_num = label(match_mask)
     print(features_num)
 
+    filtered_matches = {}
     for label_index in range(1, features_num + 1):
-        # find_label_maximum
         label_mask = labaled_array == label_index
         print(np.sum(label_mask))
 
+        label_coordinates = np.asarray(np.column_stack(np.ma.where(label_mask)))
+        scores = np.zeros_like(match_mask)
+        for y, x in label_coordinates:
+            current_matches = matches[(y, x)]
+            scores[y, x] = np.sum([match.score for match in current_matches])
+        max_score_position = np.unravel_index(np.argmax(scores), scores.shape)
+        print(max_score_position)
+        filtered_matches[max_score_position] = [matches[max_score_position], distances[max_score_position]]
+
+    print(filtered_matches)
 
 
 if __name__ == "__main__":
